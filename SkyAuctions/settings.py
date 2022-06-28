8fac839650a4667e3a3074850dbf8edc5df2beef
egoodwinx <emyrocks100@hotmail.com>
2022-06-28 19:48:49 -0400

    forgot to wrap allowed hosts in eval

--- SkyAuctions/settings.py
+++ SkyAuctions/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS =  eval(os.environ['ALLOWED_HOSTS'])
 
 
 # Application definition
